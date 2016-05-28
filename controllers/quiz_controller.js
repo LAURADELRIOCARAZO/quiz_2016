6ae071ce8f17464071e51de7617beaf85b1dcf2a
LAURADELRIOCARAZO <lauradr2@hotmail.com>
2016-05-28 14:48:20 +0200

    comentario mejorado

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -152,7 +152,18 @@ exports.edit = function(req, res, next) {
 var quiz = req.quiz;  // req.quiz: autoload de instancia de quiz
 
     res.render('quizzes/edit', {quiz: quiz});
-};
+models.User.findAll()
+	.then(function(users) {
+		var userlist = {};
+		for (var i = 0; i < users.length; i++) {
+    			userlist[users[i].id] = users[i];
+		}
+		res.render('quizzes/show', {quiz: req.quiz, answer: answer, userlist: userlist});
+	}) 
+	.catch(function(error) { next(error);}	
+	);
+ };
+ 
 
 
 
